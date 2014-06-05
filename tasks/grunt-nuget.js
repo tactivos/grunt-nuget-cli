a891cc48b47b40a213ba59f2e90e6aef4bcbc056
Johnny Halife <johnny@mural.ly>
2014-06-05 12:27:48 -0700

    added support for windows

--- tasks/grunt-nuget.js
+++ tasks/grunt-nuget.js
@@ -6,23 +6,28 @@
 
 var path = require('path');
 
-module.exports = function(grunt) {
-  grunt.registerMultiTask('nuget', 'Restores nuget packages', function() {
-    var _ = grunt.util._;
-    var done = this.async();
-    var async = grunt.util.async;
+module.exports = function (grunt) {
+    grunt.registerMultiTask('nuget', 'Restores nuget packages', function () {
+        var _ = grunt.util._;
+        var done = this.async();
+        var async = grunt.util.async;
 
-    var nugetExe = (this.options().location || path.resolve(__dirname, '../nuget/NuGet.exe'));
-    var runtimeVersion = this.options().runtimeVersion || 'v4.0';
+        var win = new RegExp(/^win/).test(process.platform);
+        var nugetExe = (this.options().location || path.resolve(__dirname, '../nuget/NuGet.exe'));
 
-    var args = []; //arguments to be used for invoking mono
-    args.push('--runtime=' + runtimeVersion);
-    args.push(nugetExe);
-    args.push(this.data.command);
+        var args = [];
 
-    if(this.data.args)
-      args.push(this.data.args);
+        if (!win) { //arguments to be used for invoking mono
+            args.push('--runtime=' + this.options().runtimeVersion || 'v4.0');
+            args.push(nugetExe);
+        }
 
-    grunt.util.spawn({ cmd: 'mono', args: args }, done);
-  });
+        args.push(this.data.command);
+
+        if (this.data.args)
+            args.push(this.data.args);
+
+        var cmd = win ? nugetExe : 'mono';
+        grunt.util.spawn({ cmd: cmd, args: args }, done);
+    });
 }
